aaf1bed8b3b4f1bde9d3a03a0d5cf64c292cf1a3
Joe Driscoll <jdriscoll7@gatech.edu>
2020-03-12 13:09:32 -0400

    Fix setup script for Debian linux.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 # Setup virtual environment.
 sudo apt install python3-venv
 python3 -m venv ./venv/
+./venv/bin/python3 -m pip install --upgrade pip
+
+# Some required packages for building.
+sudo apt install -y cmake libeigen3-dev libsuitesparse-dev qt5-default python3-dev
+
+# Install required packages.
 ./venv/bin/pip3 install -r requirements.txt
 
 # Download and install g2opy (python bindings for g2o).
@@ -12,4 +18,3 @@ cmake ..
 make -j8
 cd ..
 cp lib/g2o.cpython-36m-x86_64-linux-gnu.so ../venv/lib/python3.6/site-packages/
-
